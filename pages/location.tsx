f45e14552110bfe0646467a9bf40645d83c1e9a5
Seda Unal <sedaylmz06@outlook.com>
2023-11-30 22:15:47 +0300

    location html and css is done

---
+++ pages/location.tsx
@@ -0,0 +1,132 @@
+import React from "react";
+
+export default function Location() {
+  return (
+    <>
+      <div className="flex flex-wrap gap-4 align-center justify-center">
+        <div className=" bg-gray-50 flex flex-col justify-center relative overflow-hidden sm:py-12">
+          <div className="max-w-7xl mx-auto">
+            <div className="relative group">
+              <div className="absolute -inset-1 bg-gradient-to-r from-purple-600 to-pink-600 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
+              <div className="relative px-7 py-6 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top justify-start space-x-6">
+               <img src="./earth-svgrepo-com.svg" />
+                <div className="space-y-2">
+                  <h2 className="font-medium" >Earth C-137</h2>
+                  <p className="text-slate-800">
+                    Type : Planet
+                  </p>
+                  <p className="text-slate-800">
+                  dimension : Dimension C-137
+                  </p>
+                  <p className="text-slate-800">
+                  residents : 27 
+                  </p>
+              
+                  <a
+                    href="https://braydoncoyer.dev/blog/tailwind-gradients-how-to-make-a-glowing-gradient-background"
+                    className="block text-indigo-400 group-hover:text-slate-800 transition duration-200"
+                    target="_blank"
+                  >
+                    Read Article →
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className=" bg-gray-50 flex flex-col justify-center relative overflow-hidden sm:py-12">
+          <div className="max-w-7xl mx-auto">
+            <div className="relative group">
+              <div className="absolute -inset-1 bg-gradient-to-r from-purple-600 to-pink-600 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
+              <div className="relative px-7 py-6 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top justify-start space-x-6">
+               <img src="./earth-svgrepo-com.svg" />
+                <div className="space-y-2">
+                  <h2 className="font-medium" >Earth C-137</h2>
+                  <p className="text-slate-800">
+                    Type : Planet
+                  </p>
+                  <p className="text-slate-800">
+                  dimension : Dimension C-137
+                  </p>
+                  <p className="text-slate-800">
+                  residents : 27 
+                  </p>
+              
+                  <a
+                    href="https://braydoncoyer.dev/blog/tailwind-gradients-how-to-make-a-glowing-gradient-background"
+                    className="block text-indigo-400 group-hover:text-slate-800 transition duration-200"
+                    target="_blank"
+                  >
+                    Read Article →
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className=" bg-gray-50 flex flex-col justify-center relative overflow-hidden sm:py-12">
+          <div className="max-w-7xl mx-auto">
+            <div className="relative group">
+              <div className="absolute -inset-1 bg-gradient-to-r from-purple-600 to-pink-600 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
+              <div className="relative px-7 py-6 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top justify-start space-x-6">
+               <img src="./earth-svgrepo-com.svg" />
+                <div className="space-y-2">
+                  <h2 className="font-medium" >Earth C-137</h2>
+                  <p className="text-slate-800">
+                    Type : Planet
+                  </p>
+                  <p className="text-slate-800">
+                  dimension : Dimension C-137
+                  </p>
+                  <p className="text-slate-800">
+                  residents : 27 
+                  </p>
+              
+                  <a
+                    href="https://braydoncoyer.dev/blog/tailwind-gradients-how-to-make-a-glowing-gradient-background"
+                    className="block text-indigo-400 group-hover:text-slate-800 transition duration-200"
+                    target="_blank"
+                  >
+                    Read Article →
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className=" bg-gray-50 flex flex-col justify-center relative overflow-hidden sm:py-12">
+          <div className="max-w-7xl mx-auto">
+            <div className="relative group">
+              <div className="absolute -inset-1 bg-gradient-to-r from-purple-600 to-pink-600 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
+              <div className="relative px-7 py-6 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top justify-start space-x-6">
+               <img src="./earth-svgrepo-com.svg" />
+                <div className="space-y-2">
+                  <h2 className="font-medium" >Earth C-137</h2>
+                  <p className="text-slate-800">
+                    Type : Planet
+                  </p>
+                  <p className="text-slate-800">
+                  dimension : Dimension C-137
+                  </p>
+                  <p className="text-slate-800">
+                  residents : 27 
+                  </p>
+              
+                  <a
+                    href="https://braydoncoyer.dev/blog/tailwind-gradients-how-to-make-a-glowing-gradient-background"
+                    className="block text-indigo-400 group-hover:text-slate-800 transition duration-200"
+                    target="_blank"
+                  >
+                    Read Article →
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+     
+     
+      </div>
+    </>
+  );
+}
